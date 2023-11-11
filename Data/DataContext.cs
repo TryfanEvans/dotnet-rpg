6d96b5b2fcd5ebb399be0faf3fcfbfe575551f56
tryfanevans <tryfanle@gmail.com>
2023-11-11 15:56:44 +1030

    migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace dotnet_rpg.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Character> Characters => Set<Character>();
+
     }
 }
